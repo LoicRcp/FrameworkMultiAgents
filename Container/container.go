9e094b4004ef6f5112ca9b4b4f30a288a04dfe4c
loicr <loic.recoupe@gmail.com>
2024-02-12 14:10:44 +0100

    Container initialisation

--- Container/container.go
+++ Container/container.go
@@ -1,9 +1,31 @@
 package Container
 
-import "FrameworkMultiAgents/Agent"
+import (
+	"FrameworkMultiAgents/Agent"
+)
 
 type container struct {
-	id           string
-	agents       map[string]Agent.Agent // map
-	serverAdress string
+	// id = ipAdress
+	id               string
+	port             string
+	agents           map[string]Agent.Agent
+	mainServerAdress string // null if the container is the main container
+	mainServerPort   string
+}
+
+func NewContainer(ID, mainAdress, mainPort, port string) *container {
+	return &container{
+		id:               ID,
+		port:             port,
+		agents:           make(map[string]Agent.Agent),
+		mainServerAdress: mainAdress,
+		mainServerPort:   mainPort,
+	}
+}
+
+func (container *container) AddAgent(agent Agent.Agent) {
+	// ask the yellowPage for an ID
+
+	// add the agent to the map
+	container.agents[agent.ID] = agent
 }
