2c5285018ad0b06f4cea4d1f41118ccea5e1de33
loicr <loic.recoupe@gmail.com>
2024-03-11 19:22:32 +0100

    added main test

--- main.go
+++ main.go
@@ -1,5 +1,26 @@
-package FrameworkMultiAgents
+package main
+
+import (
+	"FrameworkMultiAgents/Container"
+	"FrameworkMultiAgents/Messages"
+	"fmt"
+)
+
+type BasicBehaviour struct{}
+
+func (b *BasicBehaviour) Perceive() {}
+func (b *BasicBehaviour) Decide()   {}
+func (b *BasicBehaviour) Act()      {}
+func (b *BasicBehaviour) HandleMailboxMessage(msg Messages.Message) {
+	fmt.Println("Message reçu dans le conteneur principal:", msg.Content)
+}
+func (b *BasicBehaviour) HandleSyncCommunication(msg Messages.Message) {}
 
 func main() {
 
+	mainContainer := Container.NewMainContainer("localhost:8080")
+
+	mainContainer.AddAgent()
+
+	mainContainer.Start()
 }
