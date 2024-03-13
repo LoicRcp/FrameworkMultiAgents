7c14d22383fec75ac796d80bbdca9ffa7c08e95c
loicr <loic.recoupe@gmail.com>
2024-03-14 00:50:18 +0100

    POC: Ping-Pong sync local

--- main.go
+++ main.go
@@ -16,14 +16,19 @@ func (b *BasicBehaviour1) Perceive(agent *Agent.Agent, params ...interface{}) {}
 func (b *BasicBehaviour1) Decide(agent *Agent.Agent, params ...interface{})   {}
 func (b *BasicBehaviour1) Act(agent *Agent.Agent, params ...interface{}) {
 	if b.tick == 0 {
-		agent.SendMail(Messages.Message{
-			Type:           Messages.InterAgentAsyncMessage,
-			Sender:         fmt.Sprintf("%d", agent.ID),
-			ContentType:    Messages.InterAgentAsyncMessageContent,
-			Content:        "Start !",
-			ExpectResponse: false,
-		}, 2)
+		err := agent.StartSyncCommunication(2)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		agent.SendSyncMessage(Messages.Message{
+			Type:        Messages.InterAgentSyncMessage,
+			Sender:      fmt.Sprintf("%d", agent.ID),
+			ContentType: Messages.InterAgentSyncMessageContent,
+			Content:     "Let's start !",
+		})
 	}
+	fmt.Printf("AGENT %d: Tick %d\n", agent.ID, b.tick)
 	b.tick++
 }
 func (b *BasicBehaviour1) HandleMailboxMessage(agent *Agent.Agent, msg Messages.Message) {
@@ -47,7 +52,27 @@ func (b *BasicBehaviour1) HandleMailboxMessage(agent *Agent.Agent, msg Messages.
 	fmt.Printf("AGENT %s: Message recu: %s\n", fmt.Sprintf("%d", agent.ID), msg.Content)
 	b.tick++
 }
-func (b *BasicBehaviour1) HandleSyncCommunication(agent *Agent.Agent, msg Messages.Message) {}
+func (b *BasicBehaviour1) HandleSyncCommunication(agent *Agent.Agent, msg Messages.Message) {
+	if msg.Content == "Ping" && b.tick < 10 {
+		agent.SendSyncMessage(Messages.Message{
+			Type:        Messages.InterAgentSyncMessage,
+			Sender:      fmt.Sprintf("%d", agent.ID),
+			ContentType: Messages.InterAgentSyncMessageContent,
+			Content:     "Pong",
+		})
+	} else if b.tick >= 10 {
+		agent.SendSyncMessage(Messages.Message{
+			Type:        Messages.InterAgentSyncMessage,
+			Sender:      fmt.Sprintf("%d", agent.ID),
+			ContentType: Messages.InterAgentSyncMessageContent,
+			Content:     "Stop !",
+		})
+	} else {
+		agent.StopSynchronousCommunication()
+	}
+	fmt.Printf("AGENT %d: Message synchrone recu : %s\n", agent.ID, msg.Content)
+	b.tick++
+}
 
 type BasicBehaviour2 struct{}
 
@@ -68,7 +93,19 @@ func (b *BasicBehaviour2) HandleMailboxMessage(agent *Agent.Agent, msg Messages.
 	fmt.Printf("AGENT %s: Message recu: %s\n", fmt.Sprintf("%d", agent.ID), msg.Content)
 
 }
-func (b *BasicBehaviour2) HandleSyncCommunication(agent *Agent.Agent, msg Messages.Message) {}
+func (b *BasicBehaviour2) HandleSyncCommunication(agent *Agent.Agent, msg Messages.Message) {
+	if msg.Content == "Pong" || msg.Content == "Let's start !" {
+		agent.SendSyncMessage(Messages.Message{
+			Type:        Messages.InterAgentSyncMessage,
+			Sender:      fmt.Sprintf("%d", agent.ID),
+			ContentType: Messages.InterAgentSyncMessageContent,
+			Content:     "Ping",
+		})
+	} else if msg.Content == "Stop !" {
+		agent.StopSynchronousCommunication()
+	}
+	fmt.Printf("AGENT %d: Message synchrone recu : %s\n", agent.ID, msg.Content)
+}
 
 func main() {
 
